37bcdd206b9327d8beb104e63a33678db84ab366
meriem-majdoul <majdoulmeriem2001@gmail.com>
2025-04-20 19:56:04 +0100

    20/04/2025_last_commit

--- src/components/DashboardMenu.js
+++ src/components/DashboardMenu.js
@@ -8,53 +8,52 @@ import Analytics from '../screens/Dashboard/Analytics';
 import Notifications from '../screens/Dashboard/Notifications';
 import Shortcuts from '../screens/Dashboard/Shortcuts';
 import Tasks from '../screens/Dashboard/Tasks';
-// import {faChartBar} from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faChartBar, faClipboardCheck, faBell, faArrowAltCircleRight } from '@fortawesome/fontawesome-svg-core';
-
+import { faChartBar, faClipboardCheck, faBell, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 
 const DashboardMenu = ({ ...props }) => {
   const StatsRoute = () => <Analytics navigation={props.navigation} />;
-  const TasksRoute = () => <Tasks navigation={props.navigation} />;
-  const NotificationsRoute = () => <Notifications navigation={props.navigation} />;
-  const ShortcutsRoute = () => <Shortcuts navigation={props.navigation} />;
+  const TasksRoute = () => <Tasks navigation={props.navigation} />
+  const NotificationsRoute = () => <Notifications navigation={props.navigation} />
+  const ShortcutsRoute = () => <Shortcuts navigation={props.navigation} />
 
-  const [index, setIndex] = React.useState(0);
+  const [index, setIndex] = React.useState(3);
   const [routes] = React.useState([
-    { key: 'stats', title: 'Stats', icon: () => <FontAwesomeIcon icon={faChartBar} size={24} color="#061357" /> },
-    { key: 'tasks', title: 'Tâches', icon: () => <FontAwesomeIcon icon={faClipboardCheck} size={24} color="#061357" /> },
-    { key: 'notifications', title: 'Notifications', icon: () => <FontAwesomeIcon icon={faBell} size={24} color="#061357" /> },
-    { key: 'shortcuts', title: 'Raccourcis', icon: () => <FontAwesomeIcon icon={faArrowAltCircleRight} size={24} color="#061357" /> },
-  ]);
+    { key: 'stats', title: 'Stats', icon: {faChartBar} },
+    { key: 'tasks', title: 'Tâches', icon: 'clipboard-check-outline' },
+    { key: 'notifications', title: 'Notifications', icon: 'bell' },
+    { key: 'shortcuts', title: 'Raccourcis', icon: 'arrow-decision' },
+  ])
 
   const renderScene = BottomNavigation.SceneMap({
     stats: StatsRoute,
     tasks: TasksRoute,
     notifications: NotificationsRoute,
     shortcuts: ShortcutsRoute,
-  });
+  })
 
   return (
+    // <Provider>
     <BottomNavigation
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
       barStyle={{ backgroundColor: '#fff', height: 55 }}
-      activeColor={'#061357'}
+      activeColor={"#061357"}
       safeAreaInset={{ bottom: 0 }}
-      renderLabel={({ route }) => (
-        <Text style={[theme.customFontMSregular.caption]}>{route.title}</Text>
-      )}
+      labelMaxFontSizeMultiplier={1.5}
+   //   renderLabel={(label)=><Text style={[theme.customFontMSregular.caption]}>{label.route.title}</Text>}
     />
-  );
-};
+    // </Provider>
+  )
+}
 
 const styles = StyleSheet.create({
   title: {
     margin: 10,
+    fontSize: 15,
     textAlign: 'center',
-    fontSize: 35,
-  },
+    fontSize: 35
+  }
 });
-
 export default DashboardMenu;
